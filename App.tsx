b230e76d46d52820391004c6d79ce521682275c3
SAYEM5522 <md1040582@gmail.com>
2021-10-01 16:10:14 +0600

    first commit

--- App.tsx
+++ App.tsx
@@ -1,23 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-
+import { Text, View } from 'react-native';
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+ 
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <View>
+        <Text>hello</Text>
+      </View>
     );
   }
-}
+
